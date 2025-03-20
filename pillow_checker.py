96877d2bd5c37d7f8c1b1fbac9af263dbd947892
Richard Pisarski <richard.pisarski@PisarskiAntenna-Group-3.local>
2025-03-20 14:00:52 -0400

    added pillowchecker file

---
+++ pillow_checker.py
@@ -0,0 +1,5 @@
+try:
+    from PIL import Image
+    print("Pillow is installed correctly.")
+except ImportError:
+    print("Pillow is not installed. Please install it using 'pip install Pillow'.") 
